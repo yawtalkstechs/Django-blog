de8036427304ac8e24e9fa8fcf8351f6f4b95a0b
yawtalkstechs <yawtalkstechs@gmail.com>
2022-02-05 14:57:50 +0000

    Blog Phase 1 Begins

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5*lu=ms*rif3$sf##_=@+ys#i4-fp5=azd*0%!^)y_pg22)+eh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
